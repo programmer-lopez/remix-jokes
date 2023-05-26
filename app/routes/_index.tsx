a106e802ad280b0d692682e652dedd6596a21804
MA514R7UBY <ing.marcoslopezsanchez@gmail.com>
2023-05-26 12:24:06 -0600

    Remix-Jokes Learning Remix

---
+++ app/routes/_index.tsx
@@ -0,0 +1,29 @@
+import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import stylesUrl from "~/routes/styles/index.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
+export default function IndexRoute() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+          <li>
+              <Link reloadDocument to="/jokes.rss">
+                RSS
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
---
+++ app/routes/jokes._index.tsx
@@ -0,0 +1,32 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    skip: randomRowNumber,
+    take: 1,
+  });
+  return json({ randomJoke });
+};
+export function ErrorBoundary() {
+  return (
+    <div className="error-container">
+      I did a whoopsies.
+    </div>
+  );
+}
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
+    </div>
+  );
+}
